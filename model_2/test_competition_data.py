366d40e7c919448e266800a7057526a9d12a3ed9
isaaclucky <isaaclucky88@gmail.com>
2024-06-05 08:05:40 +0000

    fixed bug in testing model_2

--- model_2/test_competition_data.py
+++ model_2/test_competition_data.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import sys
 from tqdm import tqdm
 from config import input_size, root, proposalN, channels, num_classes, set
-from utils.read_dataset import read_dataset
 from utils.auto_laod_resume import auto_load_resume
 from networks.model import MainNet
 import warnings
@@ -20,6 +19,9 @@ DEVICE = torch.device("cuda" if CUDA else "cpu")
 from http.client import responses
 import requests
 import json
+from datasets.dataset import CompetitionDataset
+from torch.utils.data import DataLoader
+
 
 
 def submit(results, url="https://competition-production.up.railway.app/results/"):
@@ -31,46 +33,46 @@ def submit(results, url="https://competition-production.up.railway.app/results/"
     except json.JSONDecodeError:
         print(f"ERROR: {response.text}")
         
+def create_testloader(data_dir, input_size, batch_size):
+    test_dataset = CompetitionDataset(input_size=input_size, root=data_dir, is_train=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return test_loader
 
 def test(root = './datasets/CompetitionData',labels_path='datasets/CompetitionData/labels.txt', pth_path = './models/competition.pth',num_classes = num_classes):
+    
     label_txt_file = open(labels_path)
     class_dict = {}
     for line in label_txt_file:
         class_dict[line.split(' ')[0]] = line.split(' ')[1][:-1]
 
     #load dataset
-    _, testloader = read_dataset(input_size, batch_size=1, root=root, set = set)
+    testloader = create_testloader(root, input_size, batch_size = 1)
 
-    # 定义模型
     model = MainNet(proposalN=proposalN, num_classes=num_classes, channels=channels)
 
     model = model.to(DEVICE)
     criterion = nn.CrossEntropyLoss()
-
-    #加载checkpoint
     if os.path.exists(pth_path):
         epoch = auto_load_resume(model, pth_path, status='test')
     else:
         sys.exit('The pth doesn\'t exist.')
-
+    
     print('Testing')
     object_correct = 0
     model.eval()
     preds = {}
     with torch.no_grad():
-        for i, (data, name) in enumerate(testloader):
-            x = data
-            x = x.to(DEVICE)
-            local_logits, local_imgs = model(x, epoch, i, 'test', DEVICE)[-2:]
-            # local
-            pred = local_logits.max(1, keepdim=True)[1]
-            
-            preds[name[0].split('/')[-1]] =  '0' + class_dict[str(pred.cpu().numpy()[0][0])]
-    
+        for i, data in enumerate(tqdm(testloader)):
+                x, name = data
+                x = x.to(DEVICE)
+                local_logits, local_imgs = model(x, epoch, i, 'test', DEVICE)[-2:]
+                # local
+                pred = local_logits.max(1, keepdim=True)[1]
+                preds[name[0].split('/')[-1]] =  '0' + class_dict[str(pred.cpu().numpy()[0][0])]
+
     res = {
     "images": preds,
     "groupname": "duck_duck_goose"
     }
     submit(res)
-
     return preds
--- model_2/utils/read_dataset.py
+++ model_2/utils/read_dataset.py
@@ -37,8 +37,8 @@ def read_dataset(input_size, batch_size, root, set):
                                                   shuffle=True, num_workers=8, drop_last=False)
         print('Loading Competition testset')
         testset = CompetitionDataset(input_size=input_size, root=root, is_train=False)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=8, drop_last=False)
+        testloader = torch.utils.data.DataLoader(testset,
+                                                 shuffle=False, drop_last=False)
     else:
         print('Please choose supported dataset')
         os._exit()
